cac5626444708ad2df2ff9e79a1acc4b04424e0c
mukeshraj <mukeshraj338@gmail.com>
2026-09-23 15:42:40 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,51 +1,42 @@
 #!/bin/bash
 
 AMI_ID="ami-0220d79f3f480ecf5"
-ZONE_ID="Z076627518YDYQ0034ZJQ"   #repalce with zone id
-DOMAIN_NAME="raj03.sbs"   #repalce with your domain name
-
-
+ZONE_ID="Z076627518YDYQ0034ZJQ" # replace with your zone ID
+DOMAIN_NAME="raj03.sbs" # replace with your domain name
 
 for instance in $@
 do
-    #1.Instance lauch, 2.creating route53 records,
-    #get the IP address, if frontend, we need pubilc,
-    #ex: raj03.sbs -> frontend public ip
-    #else, component-name.raj03.sbs -> private ip.
-    echo "Launching instance: $instance"    
-    INSTANCE_ID=$(aws ec2 run-instances \       #creating instances
-        --image-id ami-0220d79f3f480ecf5 \          #taking the image id
+    echo "Launching instance: $instance"
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id ami-0220d79f3f480ecf5 \
         --instance-type t3.micro \
-        --security-groups "roboshop-common" "roboshop-$instance" \  #security group name.
+        --security-groups "roboshop-common" "roboshop-$instance" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
-        --query 'Instances[0].InstanceId' \         #InstanceId is standard OUTPUT, it won't change. 
-        --output text                               #search: aws cli launch instance and get instance id.
-
+        --query 'Instances[0].InstanceId' \
+        --output text
     )
-    
     echo "Instance ID: $INSTANCE_ID"
 
     if [ $instance == "frontend" ]; then
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \       #search:aws cli to describe instance ip address. 
-        --query 'Reservations[*].Instances[*].PublicIpAddress' \
-        --output text   
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+         --query 'Reservations[*].Instances[*].PublicIpAddress' \
+         --output text
+        )
         R53_RECORD="$DOMAIN_NAME"
-        )
     else
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-        --query 'Reservations[*].Instances[*].PrivateIpAddress' \
-        --output text 
-        R53_RECORD="$instance.$DOMAIN_NAME"     #EX: if mongodb then mongodb.raj03.sbs
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+         --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+         --output text
         )
+        R53_RECORD="$instance.$DOMAIN_NAME"
     fi
-
-    #### UPDATING ROUTE53 RECORDS #####
-    #search: aws cli to update route 53 record.
+    
+    #### Updating R53 Record ####
     aws route53 change-resource-record-sets \
-    --hosted-zone-id "$ZONE_ID" \
+    --hosted-zone-id $ZONE_ID \
     --change-batch '
         {
-            "Comment": "Updating A record to a new IP",
+            "Comment": "Update A record to new IP",
             "Changes": [
                 {
                     "Action": "UPSERT",
@@ -64,5 +55,5 @@ do
         }
     '
 
-
 done
+ 
