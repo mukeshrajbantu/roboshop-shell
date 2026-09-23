891ef1af23dbebd53a1576db3039b4e2054a51ff
mukeshraj <mukeshraj338@gmail.com>
2026-09-24 00:50:13 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -8,11 +8,27 @@ for instance in $@
 do
     echo "Launching instance: $instance"
     INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id ami-0220d79f3f480ecf5 \
-    --instance-type t3.micro \
-    --security-groups "roboshop-common" "roboshop-$instance" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
-    --query 'Instances[0].InstanceId' \
-    --output text\
+        --image-id ami-0220d79f3f480ecf5 \
+        --instance-type t3.micro \
+        --security-groups "roboshop-common" "roboshop-$instance" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text\
     )
+    echo "Instance ID: $INSTANCE_ID"
+
+    if [ $instance == "frontend"]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+        --query "Reservations[*].Instances[*].PublicIpAddress" \ 
+        --output text
+        )
+        R53_RECORD="$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+        --query "Reservations[*].Instances[*].PrivateIpAddress" \ 
+        --output text
+        )
+        R53_RECORD="$instance.$DOMAIN_NAME"
+    fi
+    
 done 
