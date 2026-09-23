f7c0ad7d0344390bf59024140e2096e26531fdc9
mukeshraj <mukeshraj338@gmail.com>
2026-09-24 02:36:11 +0530

    shell-roboshop

--- 03-mysql.sh
+++ 03-mysql.sh
@@ -27,14 +27,12 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y &>> $LOGS_FILE
-dnf module enable redis:7 -y &>> $LOGS_FILE
-dnf install redis -y &>> $LOGS_FILE
-VALIDATE $? "Installing Redis:7"
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing MYSQL Server"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Allowing remote connections"
+systemctl enable mysqld
+systemctl start mysqld
+VALIDATE $? "Enabling and starting MYSQL server"
 
-systemctl enable redis &>> $LOGS_FILE
-systemctl start redis &>> $LOGS_FILE
-VALIDATE $? "Started Redis"
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up root password"
