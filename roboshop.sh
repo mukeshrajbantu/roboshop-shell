5ff30ffd19ceadb3273a89156fe727a71a9e7533
mukeshraj <mukeshraj338@gmail.com>
2026-09-23 16:18:53 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -8,13 +8,15 @@ for instance in $@
 do
     echo "Launching instance: $instance"
     INSTANCE_ID=$(aws ec2 run-instances \
-         --image-id ami-0220d79f3f480ecf5 \
-        --instance-type t3.micro \
-        --security-groups "roboshop-common" "roboshop-$instance" \ 
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
-        --query 'Instances[0].InstanceId' \
-        --output text
+    --image-id ami-0220d79f3f480ecf5 \
+    --instance-type t3.micro \
+    --security-groups "roboshop-common" "roboshop-$instance" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text
+
     )
+
     echo "Instance ID: $INSTANCE_ID"
 
     if [ $instance == "frontend" ]; then
